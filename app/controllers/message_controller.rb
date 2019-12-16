a3c276e3cb5c1c439cd38a685c468fb5ea6aed76
kunihei <kunihei@users.noreply.github.com>
2019-12-16 15:15:55 +0900

    背景の実装

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,2 +1,18 @@
 class MessageController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Messages.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:title, :text, :image)
+  end
 end
