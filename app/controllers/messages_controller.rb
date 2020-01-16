793445fd450268538354c7d5cb1e7e3ccf15bb08
kunihei <kunihei@users.noreply.github.com>
2020-01-16 16:13:39 +0900

    軽微な修正

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,19 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+    redirect_to root_path
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :text, :image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "message#index"
-  post 'message/new' => 'message#index'
-  resources :message, only: [:index, :new, :create]
+  root to: "messages#index"
+  resources :messages, only: [:index, :new, :create]
 end
