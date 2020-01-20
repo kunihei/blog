749a678d3975e28f3f918abcf4aa41088a5dc89f
kunihei <kunihei@users.noreply.github.com>
2020-01-20 16:42:28 +0900

    新規ユーザー登録ビュー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_083429) do
+ActiveRecord::Schema.define(version: 2020_01_16_105943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_083429) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
