417e94e34129e7d73f13e5b5148beca064ce47ad
kunihei <kunihei@users.noreply.github.com>
2020-02-03 13:33:09 +0900

    軽微な修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,11 @@
 class ApplicationController < ActionController::Base
-  include UserAgent
   protect_from_forgery with: :exception
   
-  before_action :detect_device_variant
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
-  end
-
-  def detect_device_variant
-    request.variant = :smart if is_mobile?
   end
 end
--- app/models/concerns/user_agent.rb
+++
@@ -1,7 +0,0 @@
-module UserAgent
-  extend ActiveSupport::Concern
-
-  def is_mobile?
-    request.user_agent =~ /iPhone|iPad|Android/
-  end
-end
